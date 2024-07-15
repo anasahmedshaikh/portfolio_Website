7243ed6518257f4ee281affa2b23dddc2f7ae4a2
anasahmedshaikh <156065391+anasahmedshaikh@users.noreply.github.com>
2024-07-15 14:31:29 +0500

    Update abc.py

--- abc.py
+++ abc.py
@@ -13,9 +13,9 @@ genai.configure(api_key=api_key)
 model = genai.GenerativeModel('gemini-1.5-flash')
 
 
-# def css(file_name):
-#     with open(file_name) as f:
-#         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+def css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 
 # css("assets/style.css")
