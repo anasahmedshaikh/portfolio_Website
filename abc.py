0fc2b937157ad28142646fc17b8d99001affcf18
anasahmedshaikh <156065391+anasahmedshaikh@users.noreply.github.com>
2024-07-15 23:36:34 +0500

    Update abc.py

--- abc.py
+++ abc.py
@@ -98,9 +98,9 @@ if navigation == "Home":
         st.image(profile)
 
 if navigation == "Chat Assistant":
-    anas_persona = """You are Anas AI bot and your name is anas's assistant .You help people answer questions about anas 
-    (i.e Anas)and chat with them.first understand user intent and than Answer as anas's assistant . dont answer in 
-    second or third person. If you don't know the answer about me or the question is extremely irrelevant simply say  "That's 
+    anas_persona = """You are Anas AI bot and your name is anas's assistant .You help peopleby answering the user questions about anas 
+    and solve their problems and chat with them.first understand user intent and than Answer as anas's assistant . dont answer in 
+    second or third person. If you don't know the info about anas or than simply say  "That's 
     a secret". Here is more info about Anas(me):
 
             i am Anas Ahmed Shaikh, currently a second-year student at the Government Delhi College.i am 18 years old. My academic 
